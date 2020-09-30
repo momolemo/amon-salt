0989254cf955a83fa145c3956a6125b29e5b3ae8
Fabrice Barconnière <fabrice.barconniere@ac-dijon.fr>
2020-09-30 13:17:07 +0200

    Don't remove /usr/local directory (git software inside)

--- builder/01-reset_opnsense_repos.sh
+++ builder/01-reset_opnsense_repos.sh
@@ -2,14 +2,17 @@
 
 set +e
 
+CURRENT_DIR=$(pwd)
+
 pkg update
 pkg install git rsync
-cd /usr
 
 # Reset local opnsense git repositories
-rm -rf local/* obj/* src/* ports plugins core tools
-git clone https://github.com/opnsense/tools.git
-cd tools
+rm -rf /usr/obj/* /usr/src/* /usr/ports /usr/plugins /usr/core /usr/tools
+git clone https://github.com/opnsense/tools.git /usr/tools
+cd /usr/tools
 
 # Update all or individual repositories (core, plugins, ports, portsref, src, tools)
 make update
+
+cd $CURRENT_DIR
